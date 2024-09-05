0efbb0400298994f86be59975e0eb8f5748ce818
rahul <rahul@aospa.co>
2024-09-05 23:15:40 +0100

    cupid: Drop kernel leftovers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,21 +15,6 @@ DEVICE_PATH := device/xiaomi/cupid
 # Bootloader
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/configs/board/board-info.txt
 
-# Kernel
-device_second_stage_modules := \
-    goodix_fod.ko \
-    qcom_pm8008-regulator.ko \
-    fts_touch_spi.ko \
-    aw8697-haptic.ko
-
-device_vendor_dlkm_exclusive_modules := \
-    cs35l41_dlkm.ko
-
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
-BOARD_VENDOR_KERNEL_MODULES_LOAD += $(device_second_stage_modules) $(device_vendor_dlkm_exclusive_modules)
-
-BOOT_KERNEL_MODULES += $(device_second_stage_modules)
-
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
